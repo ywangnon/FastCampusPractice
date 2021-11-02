db48f2aed7332cb794a197f908f78c590e150bcc
ywangnon <ywangnon123@gmail.com>
2021-11-02 20:41:51 +0900

    Feat: add chapter2 project
    MyAlbum 프로젝트 생성
    2-1 ~ 2-2까지

---
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by Hansub Yoo on 2021/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
